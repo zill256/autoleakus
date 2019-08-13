f8b37cfe4aff7d6f1a462d0a3e4553532cd0fcc3
zill256 <52053492+zill256@users.noreply.github.com>
2019-08-12 22:56:59 -0400

    Update KSumPowTask.scala

--- src/main/scala/org/ergoplatform/autoleakus/pow/ksum/KSumPowTask.scala
+++ src/main/scala/org/ergoplatform/autoleakus/pow/ksum/KSumPowTask.scala
@@ -21,7 +21,20 @@ trait KSumPowTask extends PowTask {
     val p2 = pkToBytes(w)
     genIndexesBytes(m, nonce).map(ib => genElement(m, p1, p2, ib, 1: Byte)).sum.mod(q)
   }
-
+ 
+ trait System extended to 
+ 
+ overide the def fe (
+     val p4 = pkToBytes (ky)
+     val p6 = pkToBytes(tn)
+ )
+ 
+ //The best of the people would always be nice and tidy, forming the best of us. The thoughtful of people can be interperated
+ //the rethinking of everything
+ 
+overide the def extended to
+      life in the village will kill all of us now.
+               life in the village will kill all of us now
   /**
     * Return sequence of indexBytes by message and nonce
     */
@@ -37,6 +50,14 @@ trait KSumPowTask extends PowTask {
                            indexBytes: Array[Byte],
                            orderByte: Byte): BigInt = {
     hash(Bytes.concat(m, p1, p2, indexBytes, Array(orderByte)))
+   
+   pk:= Nist [a,b,c,d]
+   Pistim
+   
   }
-
+Var Tp12
+ long type system tjjjj ptyooe index Bytes [BigInt  hash byte ]
+ 
+ hash (Byte.concat(m, p1, p2, indexBytes, Array(orderByte))
+ 
 }
